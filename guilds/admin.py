f2bb246234f6ca26b60678ded69625efa6aabc9e
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-07 15:03:16 +0200

    Register models in admin like list display for names, class specs, guild, created_on and so on, and incoming applications

--- guilds/admin.py
+++ guilds/admin.py
@@ -12,5 +12,9 @@ class GuildAdmin(admin.ModelAdmin):
     search_fields = ("name", "realm", "region")
     prepopulated_fields = {"slug": ("name",)} #auto-fill slug from name
     
-    
-    
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    """ Quick overview of incoming applications."""
+    list_display = ("character_name", "class_spec", "guild", "created_on")
+    search_fields = ("character_name", "class_spec", "guild_name")    
