d29577b4e07c1bdd1a8f7025d9b3c702d8f38e05
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-07 14:59:42 +0200

    added class guildadmin to admin.py section, list key fields make slug auto-fill

--- guilds/admin.py
+++ guilds/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Guild, Application
 
 # Register your models here.
+@admin.register(Guild)
+
+class GuildAdmin(admin.ModelAdmin):
+    """List key fields and make slug auto-fill from name."""
+    
+    list_display = ("name", "faction", "region", "realm", "published",)
+    list_filter = ("faction", "published", "region")
+    search_fields = ("name", "realm", "region")
+    prepopulated_fields = {"slug": ("name",)} #auto-fill slug from name
+    
+    
+    
