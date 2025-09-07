c52c32a1a139cf7dc6161fbf5b7012b64d34eca4
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-07 18:25:11 +0200

    Move base.html to templates/ so extends works

--- guilds/urls.py
+++ guilds/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.guild_list, name="guild_list"),
     path("guilds/", views.guild_list, name="guild_list"),   # list page
     path("guilds/<slug:slug>/", views.guild_detail, name="guild_detail"), # detail page
     path("apply/<int:guild_id>/", views.application_create, name="application_create") # apply form
