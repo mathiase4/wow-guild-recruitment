0a701dd0af46a12d308f8b0e9d865d5ab0fb935c
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-07 16:22:53 +0200

    created guilds/views.py guild_list request

--- guilds/views.py
+++ guilds/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Guild
+from .forms import ApplicationForm
+
+def guild_list(request):
+    """Public list of published guilds, wth a simple text search."""
+    q = request.GET.get("q","").strip()
+    guilds = Guild.objects.filter(published=True)
+    if q:
+        guilds = guilds.filter(name_icontains=q)
+    context = {"guilds": guilds, "q": q}
+    return render(request, "guilds/guild_list.html", context)
 
 # Create your views here.
