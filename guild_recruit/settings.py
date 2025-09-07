1db4680d340eeb37ee936f6be7986fc4423584eb
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-07 15:45:39 +0200

    switch db to Postgres on heroku when Database_url is set

--- guild_recruit/settings.py
+++ guild_recruit/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
     }
 }
 
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
