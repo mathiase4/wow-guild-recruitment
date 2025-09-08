bb6ccdcc88a6d25f48abfffb0446f883015c7224
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-08 15:18:10 +0200

    add URL route for editing a guild

--- guilds/urls.py
+++ guilds/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("guilds/", views.guild_list, name="guild_list_alt"),   # list page
     path("create/", views.guild_create, name="guild_create"),
     path("guilds/<slug:slug>/", views.guild_detail, name="guild_detail"), # detail page
+    path("guilds/<slug:slug>/edit/", views.guild_edit, name="guild_edit"), # edit page
     path("apply/<int:guild_id>/", views.application_create, name="application_create") # apply form
     
     
