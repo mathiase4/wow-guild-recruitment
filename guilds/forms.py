284c0b7bd19d5788d540584355e7978d32c583dd
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-08 13:00:55 +0200

    add GuildForm for creating ads

--- guilds/forms.py
+++ guilds/forms.py
@@ -1,6 +1,6 @@
 """ Simple forms for the guilds app."""
 from django import forms
-from .models import Application
+from .models import Application, Guild
 
 class ApplicationForm(forms.ModelForm):
     """form to apply to a guild.
@@ -9,3 +9,16 @@ class ApplicationForm(forms.ModelForm):
     class Meta:
         model = Application
         fields = ("character_name", "class_spec", "discord", "message")
+        
+        
+        
+        
+class GuildForm(forms.ModelForm):
+    """ Form based on the Guild model, for users to create and edit their guild ads."""
+    class Meta:
+        model = Guild
+        #List the fields a user can fill out
+        fields = [ 'name', 'faction', 'region', 'realm', 'description']
+        
+        
+
