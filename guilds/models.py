c594b116f0d00792e9428ac84c30c5fdc959d8a3
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-08 13:50:51 +0200

    fix: Correct indentation of save method in Guild model

--- guilds/models.py
+++ guilds/models.py
@@ -35,10 +35,13 @@ class Guild(models.Model):
         """Show a friendly name in admin, logs and shells.""" 
         return self.name
     
-def save(self, *args, **kwargs):
-    """ auto-generate a slug from the guild name if one doesn't exist."""
-    if not self.slug:
-        self.slug = slugify(self.name)
+    def save(self, *args, **kwargs):
+        """
+        Auto-generate a slug from the guild name if one doesn't exist.
+        """
+        if not self.slug:
+            self.slug = slugify(self.name)
+        
         super().save(*args, **kwargs)
     
     # Application model
