b9651d075f72faf24e0b47d42cc077d296aafb09
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-11 18:42:57 +0200

    git push for update in readme file on github

--- guild_recruit/settings.py
+++ guild_recruit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-change-me")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', "localhost", ".herokuapp.com"]
 CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
